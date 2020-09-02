964478eb9065edb227bf7279f757d4cdaa051167
maria laura villegas rincon <malauravill@gmail.com>
2020-09-02 13:19:49 -0300

    mi primer commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estructura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Laura";
+            Console.WriteLine(nombre);
+            Console.ReadLine();
+        }
+    }
+}
